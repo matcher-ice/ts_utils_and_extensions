3ec6f67ae27102c291a471269a0301e7ba541156
matcher <masch9131@gmail.com>
2020-08-30 10:15:40 +0900

    Modifying Multiplet

--- Multiplet.ts
+++ Multiplet.ts
@@ -3,4 +3,4 @@ type MultipletRecursive<T, N, TArgs extends T[], TReturn extends T[]> = {
     "f": MultipletRecursive<T, N, [T, ...TArgs], [T, ...TReturn]>;
 }[TReturn extends { length: N } ? "t" : "f"];
 
-export type Mutilplet<T, N extends number> = MultipletRecursive<T, N, T[], []>;
+export type Multiplet<T, N extends number> = MultipletRecursive<T, N, T[], []>;
